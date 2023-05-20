56ac8151eafc038f335c822d6b26743cbf190371
Claudio Loseto <loseto@pm.me>
2023-05-20 15:44:30 +0200

    Add list of apps to windows installation

--- windows/install-apps.ps1
+++ windows/install-apps.ps1
@@ -1,5 +1,20 @@
 $AppList = @(
-
+    'Streamlabs.Streamlabs'
+    'AgileBits.1Password'
+    '7zip.7zip'
+    'Brave.Brave'
+    'Discord.Discord'
+    'Spotify.Spotify'
+    'Valve.Steam'
+    'VideoLAN.VLC'
+    'Battle.net'
+    'qBittorrent.qBittorrent'
+    'SlackTechnologies.Slack'
+    'Nvidia.GeForceExperience'
+    'Nvidia.Broadcast'
+    'EpicGames.EpicGamesLauncher'
+    'Telegram.TelegramDesktop'
+    'MullvadVPN.MullvadVPN'
 )
 
 foreach ($appId in $AppList) {
